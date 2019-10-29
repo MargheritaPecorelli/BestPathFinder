9c7be303638940d90f1b22cff296c1365c7bed55
Margherita Pecorelli <margherita.pecorelli@studio.unibo.it>
2019-10-29 18:19:31 +0100

    first version of path finder with disability required

--- index.js
+++ index.js
@@ -13,38 +13,116 @@ const GetNewFactHandler = {
     return request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    const speechOutput = 'Welcome to Unibo path finder! What can I do for you?';
+    const speechOutput = 'Benvenuto nel Campus di Cesena! Cosa posso fare per te?';
     return handlerInput.responseBuilder
       .speak(speechOutput)
       .getResponse();
   },
 };
 
+
+//questo viene richiamato quando non è presente il valore di "disabilita"
+const StartedPathFinderIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === "IntentRequest"
+      && request.intent.name === "PathFinderIntent"
+      && request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .addDelegateDirective()
+      .getResponse();
+  }
+}
+
+//questo viene richiamato quando non sono presenti né il valore di "destinazione", né quello di "nomeProfessore", né quello di "attivita"
+//dalmomento che può essere messo un unico .addElicitSlotDirective('...'), ho deciso che nel caso in cui uno dica semplicemente la propria disabilità,
+//gli verrà chiesto dove vuole andare (non quali attività vuole fare o altro)
+const PathFinderInProgressHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === "IntentRequest"
+      && request.intent.name === "PathFinderIntent"
+      && !request.intent.slots.destinazione.value
+      && !request.intent.slots.nomeProfessore.value
+      && !request.intent.slots.attivita.value
+      && request.intent.slots.disabilita.value
+      && request.dialogState === 'COMPLETED';
+  },
+  handle(handlerInput) {
+    //const destination = handlerInput.requestEnvelope.request.intent.slots.destinazione.value; //undefined
+    //const speechText = `valore di destinazione: ${destination}`;
+    //const speechText = `valore di disabilita ${handlerInput.requestEnvelope.request.intent.slots.disabilita.value}`
+    const speechText = 'Dimmi dove devi andare, così ti dirò come arrivarci';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .addElicitSlotDirective('destinazione')
+      .getResponse();
+  }
+}
+
+//questo viene richiamato quando è presente il valore di "destinazione"
 const PathFinderWithDestinationHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' 
-    && request.intent.name === 'PathFinderIntent'
-    && handlerInput.requestEnvelope.request.intent.slots.destinazione.value;
+    return request.type === "IntentRequest"
+      && request.intent.name === "PathFinderIntent"
+      && request.intent.slots.destinazione.value
+      && request.dialogState === 'COMPLETED';
   },
   handle(handlerInput) {
     const locationName = handlerInput.requestEnvelope.request.intent.slots.destinazione.value;
-    var speechOutput = '';
-    var isDestinationPresent = false;
-    locations.forEach(item=>{
+    const disability = handlerInput.requestEnvelope.request.intent.slots.disabilita.value;
+    var speechOutput = `mi dispiace ma non conosco ${locationName}`;
+    //var isDestinationPresent = false;
+    locations.forEach(item => {
       if(locationName.includes(item)) {
-        isDestinationPresent = true;
-        speechOutput = item;
+        //isDestinationPresent = true;
+        if (disability.includes('no') || disability.includes('nesssuna')) {
+          speechOutput = `per raggiungere ${item} devi ...`;
+        } else {
+          speechOutput = `per raggiungere ${item} con disabilità ${disability}, devi ...`;
+        }
       }
     });
-    if (!isDestinationPresent) {
+    /*if (!isDestinationPresent) {
       speechOutput = `mi dispiace ma non conosco ${locationName}`;
-    }
+    }*/
     return handlerInput.responseBuilder
         .speak(speechOutput)
         .getResponse();
   },
 };
+
+//questo viene richiamato quando è presente il valore di "attivita"
+/*const PathFinderWithActivityHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === "IntentRequest"
+      && request.intent.name === "PathFinderIntent"
+      && request.intent.slots.attività.value
+  },
+  handle(handlerInput) {
+    const activityName = handlerInput.requestEnvelope.request.intent.slots.attivita.value;
+    const disability = handlerInput.requestEnvelope.request.intent.slots.disabilita.value;
+    var speechOutput = `mi dispiace ma non conosco ${activityName}`;
+    activities.forEach(item => {
+      if(activityName.includes(item)) {
+        //if (disability.includes('no') || disability.includes('nesssuna')) {
+          const activityLocation = item.location();
+          speechOutput = `${item} si tiene presso  ${activityLocation}. Vuoi sapere come raggiungerla?`;
+        //} else {
+          //speechOutput = `per raggiungere ${item} con disabilità ${disability}, devi ...`;
+        //}
+      }
+    });
+    return handlerInput.responseBuilder
+        .speak(speechOutput)
+        .getResponse();
+  },
+};*/
 
 const HelpHandler = {
   canHandle(handlerInput) {
@@ -112,6 +190,8 @@ exports.handler = skillBuilder
   .addRequestHandlers(
     GetNewFactHandler,
     PathFinderWithDestinationHandler,
+    StartedPathFinderIntentHandler,
+    PathFinderInProgressHandler,
     HelpHandler,
     ExitHandler,
     SessionEndedRequestHandler
